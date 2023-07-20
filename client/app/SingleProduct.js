3f6c8755803f741b13f966932ec98bd5ec23ef3f
richiepchavez <richiepchavez@gmail.com>
2023-07-20 12:19:41 -0700

    single product created

---
+++ client/app/SingleProduct.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const SingleProduct = () => {
+  return (
+    <div>
+      <section>
+        <div className="chip-img-container">
+          <img src='' alt='Chip Image'/>
+        </div>
+        <div>
+          <h1>
+              Chip Name
+          </h1>
+          <h2>
+              Reviews: want to figure out how to make them stars
+          </h2>
+          <p>
+              Description
+          </p>
+          <h3>
+              Image of nutrition - change to image
+          </h3>
+          <h3>
+              Ingredients
+          </h3>
+          <h3>
+              size
+          </h3>
+          <h3>Buy Now</h3>
+        </div>
+      </section>
+      <section>
+        <div>
+          Customer Reviews
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default SingleProduct;

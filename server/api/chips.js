a1373873158258d998389d7254814e86cb341bad
jangchoi <jangho91a@gmail.com>
2023-07-20 17:45:13 -0400

    routes for chips, orders. added middleware in app.js

---
+++ server/api/chips.js
@@ -0,0 +1,69 @@
+const router = require('express').Router();
+const {
+    models: { Chips },
+  } = require("../db"); 
+
+router.get('/', async (req, res, next) => {
+    try {
+      const chips = await Chips.findAll();
+      res.json(chips);
+    } catch (err) {
+      next(err);
+    }
+  });
+
+//GET individual chip
+router.get('/:chipId', async (req, res, next) => {
+    try {
+      const chip = await Chips.findByPk(req.params.chipId);
+      if(chip){
+        res.json(chip);
+      } else {
+        res.status(404).send("Chip not found");
+      }
+    } catch (err) {
+      next(err);
+    }
+});
+
+//POST new chip
+router.post('/', async (req, res, next) => {
+    try {
+      const newChip = await Chips.create(req.body);
+      res.status(201).json(newChip);
+    } catch (err) {
+      next(err);
+    }
+  });
+
+//DELETE chip
+router.delete('/:chipId', async (req, res, next) => {
+    try {
+      const chip = await Chips.findByPk(req.params.chipId);
+      if (chip) {
+        await chip.destroy();
+        res.status(204).send("Chip deleted");
+      } else {
+        res.status(404).send("Chip not found");
+      }
+    } catch (err) {
+      next(err);
+    }
+  });
+
+//PUT chip - update
+router.put('/:chipId', async (req, res, next) => {
+    try {
+      const chip = await Chips.findByPk(req.params.chipId);
+      if (chip) {
+        const updatedChip = await chip.update(req.body);
+        res.json(updatedChip);
+      } else {
+        res.status(404).send("Chip not found");
+      }
+    } catch (err) {
+      next(err);
+    }
+  });
+  
+  module.exports = router;
--- server/db/index.js
+++ server/db/index.js
@@ -18,5 +18,6 @@ module.exports = {
   models: {
     User,
     Chips,
+    Orders
   },
 };

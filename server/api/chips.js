e24b54c671f9835a0abe281fa8341fb9f58937a3
richiepchavez <richiepchavez@gmail.com>
2023-07-21 12:02:53 -0700

    accepted jays changes

--- server/api/chips.js
+++ server/api/chips.js
@@ -1,49 +1,3 @@
-<<<<<<< HEAD
-const router = require('express').Router();
-const {
-  models: { Chips },
-} = require('../db');
-const { Op } = require('sequelize');
-
-// GET localhost:3000/api/chips/
-router.get('/', async (req, res, next) => {
-  try {
-    const data = await Chips.findAll();
-    res.status(200).json(data);
-  } catch (error) {
-    console.error(error);
-  }
-});
-
-const landingPageChipId = [2, 4, 6, 8, 10, 12];
-
-// get localhost:3000/api/chips/landingPage
-router.get('/landingPage', async (req, res, next) => {
-  try {
-    const data = await Chips.findAll({
-      where: {
-        id: {
-          [Op.or]: landingPageChipId,
-        },
-      },
-    });
-    res.status(200).json(data);
-  } catch (error) {
-    console.error(error);
-  }
-});
-
-router.get('/:id', async (req, res, next) => {
-  try {
-    const singleChipInfo = await Chips.findByPk(req.params.id);
-    res.json(singleChipInfo);
-  } catch (err) {
-    next(err);
-  }
-});
-
-module.exports = router;
-=======
 
 const router = require('express').Router();
 const {
@@ -133,4 +87,3 @@ router.put('/:chipId', async (req, res, next) => {
   });
   
   module.exports = router;
->>>>>>> main

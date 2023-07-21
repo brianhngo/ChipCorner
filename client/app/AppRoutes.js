0d1310144044f163b04a871da8b6840a8aa0f3bc
Brian Ngo <brianhngo@gmail.com>
2023-07-21 12:58:53 -0700

    refactored components

--- client/app/AppRoutes.js
+++ client/app/AppRoutes.js
@@ -1,10 +1,10 @@
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { Route, Routes } from "react-router-dom";
-import AuthForm from "../features/auth/AuthForm";
-import Home from "../features/home/Home";
-import SingleProduct from "./SingleProduct";
-import { me } from "./store";
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Route, Routes, Link } from 'react-router-dom';
+import AuthForm from '../features/auth/AuthForm';
+import Home from '../features/home/Home';
+import SingleProduct from './SingleProduct';
+import { me } from './store';
 
 /**
  * COMPONENT
@@ -20,30 +20,32 @@ const AppRoutes = () => {
 
   return (
     <div>
-      {isLoggedIn ? (
-        <Routes>
-          <Route path="/chips/:id" element={<SingleProduct />} />
-          <Route path="/home" element={<Home />} />
-          
-          <Route path="/*" element={<Home />} />
-        </Routes>
-      ) : (
-        <Routes>
-          <Route
-            path="/login"
-            element={<AuthForm name="login" displayName="Login" />}
-          />
-          <Route
-            path="/signup"
-            element={<AuthForm name="signup" displayName="Sign Up" />}
-          />
-          <Route
-            path="/*"
-            element={<AuthForm name="login" displayName="Login" />}
-          />
-           <Route path="/chips/:id" element={<SingleProduct />} />
-        </Routes>
-      )}
+      <Routes>
+        <Route path="/chips/:id" element={<SingleProduct />} />
+
+        {isLoggedIn ? (
+          <>
+            <Route path="/chips/:id" element={<SingleProduct />} />
+            <Route path="/home" element={<Home />} />
+          </>
+        ) : (
+          <>
+            <Route path="/chips/:id" element={<SingleProduct />} />
+            <Route
+              path="/login"
+              element={<AuthForm name="login" displayName="Login" />}
+            />
+            <Route
+              path="/signup"
+              element={<AuthForm name="signup" displayName="Sign Up" />}
+            />
+            <Route
+              path="/*"
+              element={<AuthForm name="login" displayName="Login" />}
+            />
+          </>
+        )}
+      </Routes>
     </div>
   );
 };

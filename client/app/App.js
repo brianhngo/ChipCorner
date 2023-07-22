51232170b05d5feda325c773be139a6b801bab2d
jangchoi <jangho91a@gmail.com>
2023-07-22 18:14:59 -0400

    allchips page

--- client/app/App.js
+++ client/app/App.js
@@ -2,12 +2,14 @@ import React, { useEffect } from 'react';
 import LandingPage from './LandingPage';
 import SingleProduct from './SingleProduct';
 import { Routes, Route, Link } from 'react-router-dom';
+import FilteredChips from './FilterChips';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/chips/:id" element={<SingleProduct />} />
       <Route path="/" element={<LandingPage />} />
+      <Route path="/allchips" element={<FilteredChips/>} />
     </Routes>
   );
 };

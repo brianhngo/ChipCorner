6ab55a9e5456b291acbc00c66847b8bdaa6f8824
Brian Ngo <brianhngo@gmail.com>
2023-07-24 09:02:04 -0700

    fixed header

--- client/app/CreateNewUser.js
+++ client/app/CreateNewUser.js
@@ -4,6 +4,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { changeStatus } from './CreateNewUserSlice';
+import AppRoutes from './AppRoutes';
+import Navbar from '../features/navbar/Navbar';
 
 export default function CreateNewUser() {
   const navigate = useNavigate();
@@ -60,47 +62,69 @@ export default function CreateNewUser() {
   }, [createUserStatus]);
 
   return (
-    <div className="createNewUserContainer">
-      <h1> Create New User</h1>
-      <p> Please Enter Information to continue </p>
-      <form onSubmit={submitHandler}>
-        <label htmlFor="username">
-          <h4> Username :</h4>
-        </label>
-        <input
-          type="text"
-          id="username"
-          name="username"
-          value={username}
-          onChange={userNameHandler}
-          className={verifyStatus === true ? 'highlights' : null}></input>
+    <>
+      <div id="container">
+        <section id="headerSection">
+          <header id="headerContainer">
+            <div id="websiteTitle">
+              <h3>All Chips</h3>
+              <img
+                className="logoImage"
+                src="https://media.istockphoto.com/id/164661881/vector/nachos-cartoon.jpg?s=612x612&w=0&k=20&c=AFnAYL79XMt0VQSVHtPRTuJUR1z0Iwig8LCzC3083Ag="
+              />
+            </div>
+            <nav id="navContainer">
+              <Link to="/"> Home </Link>
+              <Link to="/allchips"> All Chips </Link>
+              <Link to="/signup">Sign Up</Link>
+              <AppRoutes />
+              <Navbar />
+            </nav>
+          </header>
+        </section>
+      </div>
+      <div className="createNewUserContainer">
+        <h1> Create New User</h1>
+        <p> Please Enter Information to continue </p>
+        <form onSubmit={submitHandler}>
+          <label htmlFor="username">
+            <h4> Username :</h4>
+          </label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={username}
+            onChange={userNameHandler}
+            className={verifyStatus === true ? 'highlights' : null}></input>
 
-        <label htmlFor="password">
-          <h4> Password : </h4>
-        </label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          value={password}
-          onChange={passwordHandler}></input>
+          <label htmlFor="password">
+            <h4> Password : </h4>
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={passwordHandler}></input>
 
-        <label htmlFor="email">
-          <h4> Email : </h4>
-        </label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={email}
-          onChange={emailHandler}
-          className={verifyStatus === true ? 'highlights' : null}></input>
-        <button> Create User! </button>
-      </form>
-      {verifyStatus === true ? (
-        <p className="error-message"> Email and Username has been used </p>
-      ) : null}
-      <Link to="/"> Back to Home Page</Link>
-    </div>
+          <label htmlFor="email">
+            <h4> Email : </h4>
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={emailHandler}
+            className={verifyStatus === true ? 'highlights' : null}></input>
+          <button> Create User! </button>
+        </form>
+        {verifyStatus === true ? (
+          <p className="error-message"> Email and Username has been used </p>
+        ) : null}
+        <Link to="/"> Back to Home Page</Link>
+      </div>
+    </>
   );
 }

36dd183b039c0c3d964f50f9558b594e8029faab
Brian Ngo <brianhngo@gmail.com>
2023-09-09 23:26:21 -0700

    attempted to fix carousel in deployed

--- client/app/LandingPage/MyCarousel.js
+++ client/app/LandingPage/MyCarousel.js
@@ -5,7 +5,7 @@ import { Carousel } from 'react-responsive-carousel';
 import image1 from '../../../public/image1.jpeg';
 import image2 from '../../../public/image2.jpeg';
 import image3 from '../../../public/image3.jpeg';
-
+ //
 import './Carousel.css'
 const MyCarousel = () => {
   return (
